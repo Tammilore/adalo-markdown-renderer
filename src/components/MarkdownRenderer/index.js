1578d6567839819b3302d7d98f63403e3c47c5a3
Tami <tammilore@gmail.com>
2024-03-08 14:45:47 +0000

    Markdown customizations

--- src/components/MarkdownRenderer/index.js
+++ src/components/MarkdownRenderer/index.js
@@ -3,7 +3,59 @@ import {View, StyleSheet} from 'react-native';
 import Markdown from 'react-native-markdown-display';
 
 const MarkdownRenderer = (props) => {
-  const { markdownText } = props
+  const { markdownText, textColor, fontSize } = props
+
+  const markdownStyles = {
+    // Bold text
+    strong: {fontWeight: 'bold'},
+  
+    // Italic text
+    em: {fontStyle: 'italic'},
+  
+    // Bold and italic
+    s: {fontWeight: 'bold', fontStyle: 'italic'}, // Adjust based on your Markdown renderer
+  
+    // Headings
+    heading1: {fontSize: 32, fontWeight: 'bold'},
+    heading2: {fontSize: 28, fontWeight: 'bold'},
+    heading3: {fontSize: 24, fontWeight: 'bold'},
+    heading4: {fontSize: 20, fontWeight: 'bold'},
+    heading5: {fontSize: 18, fontWeight: 'bold'},
+    heading6: {fontSize: 16, fontWeight: 'bold'},
+  
+    // Links
+    link: {color: 'blue', textDecorationLine: 'underline'},
+  
+    // Text
+    body: {fontSize: fontSize, color: textColor}, 
+    // text: {fontSize: fontSize, color: textColor}, // Default text style
+  
+    // Lists
+    bullet_list: {}, // Style for unordered list container
+    ordered_list: {}, // Style for ordered list container
+    list_item: { flexDirection: 'row', alignItems: 'flex-start' },
+
+    // Blockquotes
+    blockquote: {backgroundColor: '#f9f9f9', borderLeftWidth: 5, borderLeftColor: '#ccc', paddingLeft: 10},
+  
+    // Code
+    code_inline: {backgroundColor: '#f5f5f5', fontFamily: 'monospace'},
+    code_block: {backgroundColor: '#f5f5f5', fontFamily: 'monospace'},
+  
+    // Images
+    image: {width: 100, height: 100}, 
+  
+    // Tables (if supported)
+    table: { borderWidth: 1, borderColor: '#444' }, 
+    thead: {}, 
+    tbody: {}, 
+    th: { fontWeight: 'bold', padding: 8, borderWidth: 1, borderColor: '#444' }, 
+    tr: {}, 
+    td: { padding: 8, borderWidth: 1, borderColor: '#444' }, 
+  
+    // Additional custom styles as needed
+  };
+  
 
   return (
     <View style={styles.container}>
@@ -12,14 +64,13 @@ const MarkdownRenderer = (props) => {
   );
 };
 
-const markdownStyles = {
-  text: {
-    fontSize: 16,
-    color: 'white', 
-    fontWeight: 'normal',
-    fontFamily: 'Karla',
-  }
-}
+// const markdownStylesTwo = {
+//   text: {
+//     fontSize: fontSize,
+//     color: 'blue', 
+//     fontWeight: 'normal'
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
